8e209f413215e296e721d07007d861a248c4f13d
jax-b <11304426+jax-b@users.noreply.github.com>
2020-05-29 22:43:24 -0400

    Finished out the last code

--- SerialSD.go
+++ SerialSD.go
@@ -1,8 +1,9 @@
 package deejdsp
 
 import (
-	"errors"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/jax-b/deej"
 	"go.uber.org/zap"
@@ -26,44 +27,66 @@ func NewSerialSD(sio *deej.SerialIO, logger *zap.SugaredLogger) (*SerialSD, erro
 
 // ListDir lists the dir to logger and returns it as a string
 func (serSD *SerialSD) ListDir() (string, error) {
-	serSD.sio.Pause()
-	serSD.logger.Info("SDCard File list")
-	serSD.sio.WriteStringLine(serSD.logger, "deej.modules.sd.list")
-	SerialData := <-serSD.sio.ReadLine(serSD.logger)
-	returnText := SerialData
-	for SerialData != "" {
-		serSD.logger.Info(SerialData)
-		SerialData = <-serSD.sio.ReadLine(serSD.logger)
-		returnText = returnText + SerialData
+	resumeAfter := serSD.sio.IsRunning()
+
+	if serSD.sio.IsRunning() {
+		serSD.sio.Pause()
 	}
-	serSD.sio.Start()
+
+	serSD.sio.WriteStringLine(serSD.logger, "deej.modules.sd.list")
+
+	lineChannel := serSD.sio.ReadLine(serSD.logger)
+
+	SerialData := <-lineChannel
+	returnText := SerialData
+
+Loop:
+	for {
+		select {
+		case <-time.After(1 * time.Second):
+			break Loop
+		case SerialData = <-lineChannel:
+			returnText = returnText + SerialData
+		}
+	}
+	lineChannel = nil
+
+	if resumeAfter {
+		serSD.sio.Start()
+	}
 	return returnText, nil
 }
 
 // Delete deletes a file off of the SD card
-func (serSD *SerialSD) Delete(filename string) (string, error) {
-	serSD.sio.Pause()
+func (serSD *SerialSD) Delete(filename string) error {
+	resumeAfter := serSD.sio.IsRunning()
 
-	serSD.logger.Infof("Deleting %q from the SD Card", filename)
+	if serSD.sio.IsRunning() {
+		serSD.sio.Pause()
+	}
+
+	filename = strings.ToUpper(filename)
+
+	serSD.logger.Debugf("Deleting %q from the SD Card", filename)
 	serSD.sio.WriteStringLine(serSD.logger, "deej.modules.sd.delete")
 	serSD.sio.WriteStringLine(serSD.logger, filename)
 
-	success, cmdKey := serSD.sio.WaitFor(serSD.logger, "FILEDELETED")
-
-	if success == false {
-		serSD.logger.Errorw("Failed to delete file", filename)
-		return cmdKey, errors.New("Cannot delete file")
+	if resumeAfter {
+		serSD.sio.Start()
 	}
 
-	serSD.sio.Start()
-	return cmdKey, nil
+	return nil
 }
 
 // SendFile Sends a file to the sd card
 func (serSD *SerialSD) SendFile(filepath string, DestFilename string) error {
-	serSD.sio.Pause()
+	resumeAfter := serSD.sio.IsRunning()
 
-	serSD.logger.Infof("Sending %q to the SD Card with %q as the file name", filepath, DestFilename)
+	if serSD.sio.IsRunning() {
+		serSD.sio.Pause()
+	}
+
+	serSD.logger.Debugf("Sending %q to the SD Card with %q as the file name", filepath, DestFilename)
 	serSD.sio.WriteStringLine(serSD.logger, "deej.modules.sd.send")
 	serSD.sio.WriteStringLine(serSD.logger, DestFilename)
 
@@ -81,7 +104,8 @@ func (serSD *SerialSD) SendFile(filepath string, DestFilename string) error {
 	}
 	serSD.sio.WriteStringLine(serSD.logger, "EOF")
 
-	serSD.sio.Start()
-
+	if resumeAfter {
+		serSD.sio.Start()
+	}
 	return nil
 }
