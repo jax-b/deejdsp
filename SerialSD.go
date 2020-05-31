5641f20c3ed091e010f7ea4d1355779a239fda37
jax-b <11304426+jax-b@users.noreply.github.com>
2020-05-31 00:01:39 -0400

    Fixed Send File

--- SerialSD.go
+++ SerialSD.go
@@ -1,6 +1,9 @@
 package deejdsp
 
 import (
+	"bufio"
+	"io"
+	"log"
 	"os"
 	"strings"
 	"time"
@@ -71,6 +74,20 @@ func (serSD *SerialSD) Delete(filename string) error {
 	serSD.sio.WriteStringLine(serSD.logger, "deej.modules.sd.delete")
 	serSD.sio.WriteStringLine(serSD.logger, filename)
 
+	//clear status messages
+	lineChannel := serSD.sio.ReadLine(serSD.logger)
+
+Loop:
+	for {
+		select {
+		case <-time.After(250 * time.Millisecond):
+			break Loop
+		case <-lineChannel:
+		}
+	}
+
+	lineChannel = nil
+
 	if resumeAfter {
 		serSD.sio.Start()
 	}
@@ -88,22 +105,46 @@ func (serSD *SerialSD) SendFile(filepath string, DestFilename string) error {
 
 	serSD.logger.Debugf("Sending %q to the SD Card with %q as the file name", filepath, DestFilename)
 	serSD.sio.WriteStringLine(serSD.logger, "deej.modules.sd.send")
+
 	serSD.sio.WriteStringLine(serSD.logger, DestFilename)
+
+	finfo, err := os.Stat(filepath)
+	fsize := finfo.Size()
 
 	f, err := os.Open(filepath)
 	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	r := bufio.NewReader(f)
+	b := make([]byte, fsize)
+
+	n, err := r.Read(b)
+	if err == io.EOF {
 		return err
 	}
-	defer f.Close()
+	serSD.sio.WriteBytes(serSD.logger, b[0:n])
 
-	b1 := make([]byte, 1)
-	n1, err := f.Read(b1)
-	for n1 > 0 {
-		serSD.sio.WriteBytes(serSD.logger, b1)
-		n1, err = f.Read(b1)
-		time.Sleep(time.Millisecond * 2)
-	}
 	serSD.sio.WriteStringLine(serSD.logger, "EOF")
+
+	//clear status messages
+	lineChannel := serSD.sio.ReadLine(serSD.logger)
+
+Loop:
+	for {
+		select {
+		case <-time.After(250 * time.Millisecond):
+			break Loop
+		case <-lineChannel:
+		}
+	}
+
+	lineChannel = nil
 
 	if resumeAfter {
 		serSD.sio.Start()
